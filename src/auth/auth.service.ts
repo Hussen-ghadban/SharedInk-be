56060c6e65cc23ac7d04ee78f41412de0411b3d9
Hussen-ghadban <houssein.ghadban99@gmail.com>
2025-07-29 09:41:06 +0300

    intialize the project

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+
+  async signup(email: string, password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+    return { message: 'Signup successful', user: { id: user.id, email: user.email } };
+  }
+
+  async signin(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    const token = await this.jwt.signAsync({ sub: user.id, email: user.email });
+    return { access_token: token };
+  }
+}
